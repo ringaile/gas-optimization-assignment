19f759a00baa257a6a00e9184761201d57f23b7b
Ringaile Valiaugaite <ringaile@gmail.com>
2022-01-29 23:46:15 +0000

    fixed example1.sol

--- contracts/Example1.sol
+++ contracts/Example1.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.8.4;
 
 contract Example1 {
-    uint8 public day = 10;
-    uint256 public amount = 1000;
-    address public ownerAddr = 0x4e653b730367e303D30d8FB39d7350D07a60dfD6;
+    uint8 public day;
+    uint16 public amount;
+    address public ownerAddr;
+
+    constructor() {
+        day = 10;
+        amount = 1000;
+        ownerAddr = 0x4e653b730367e303D30d8FB39d7350D07a60dfD6;
+    }
 }
