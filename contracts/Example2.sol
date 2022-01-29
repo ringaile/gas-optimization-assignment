103507820802c43d44d30f55948475d19982fe16
Ringaile Valiaugaite <ringaile@gmail.com>
2022-01-29 23:49:29 +0000

    fixed example2.sol

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
+    function incrementBy(uint[] calldata arr) public {
+        uint temp;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            temp += arr[idx];
         }
+        counter +=temp;
     }
 }
