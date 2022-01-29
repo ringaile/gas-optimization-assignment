ac52ffa9747bdacf3796a0742de12e69be4bfde8
Ringaile Valiaugaite <ringaile@gmail.com>
2022-01-29 23:51:09 +0000

    fixed example3.sol

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.8.4;
 
 contract Example3 {
-    uint256 marginPercentage = 30;
+    
     function getOwnerMargin(uint256 amount)
         public
-        view
+        pure
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
+        uint8 marginPercentage = 30;
+
         amountForSender = (amount * (100 - marginPercentage)) / 100;
         amountForOwner = (amount * marginPercentage) / 100;
     }
